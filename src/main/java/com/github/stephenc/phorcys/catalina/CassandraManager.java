1402aea2964f8feee5b08e86fe54676743541313
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-06-24 09:20:40 +0100

    Initial work

---
+++ src/main/java/com/github/stephenc/phorcys/catalina/CassandraManager.java
@@ -0,0 +1,7 @@
+package com.github.stephenc.phorcys.catalina;
+
+import  org.apache.catalina.Manager;
+import org.apache.catalina.session.PersistentManagerBase;
+
+public class CassandraManager extends PersistentManagerBase {
+}
